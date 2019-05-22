09f0180a0e2b9ae2af622375a14ef757b0c2cf1f
kirito2440222510 <2440222510@qq.com>
2019-05-22 22:34:30 +0800

    Site updated: 2019-05-22 22:34:29

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -4,16 +4,20 @@ jQuery(document).ready(function($) {
         var a = new Array
         ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
-        a_idx = (a_idx + 1) % a.length;
+        var b = new Array
+        ("#7fdbff", "#39cccc", "#2ecc40", "#ffdc00", "#01ff70", "#ff851b", "#ff4136", "#b1ddc9");
+		a_idx = (a_idx + 1) % a.length;
+		b_idx = Math.floor(Math.random()*8);
         var x = e.pageX,
         y = e.pageY;
+		
         $i.css({
             "z-index": 5,
             "top": y - 20,
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#FF0000"
+            "color": b[b_idx]
         });
         $("body").append($i);
         $i.animate({
